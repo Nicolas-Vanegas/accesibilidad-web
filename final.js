434f689a425c1294e90e33b5a2e24dd796988c81
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-09-01 16:20:22 -0500

    fin curso

--- final.js
+++ final.js
@@ -1,9 +1,7 @@
 window.onload = () => {
   document.querySelector(".arrow-right").addEventListener("click", clickRight);
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
-  document
-    .querySelector(".send-button")
-    .addEventListener("click", e => validateForm(e));
+  document.querySelector(".send-button").addEventListener("click", e => validateForm(e));
   document.querySelectorAll(".project").forEach(element => {
     element.addEventListener("click", e => openModal(e));
   });
@@ -13,27 +11,25 @@ window.onload = () => {
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
 function clickRight() {
-  const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
-    10
-  );
-  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
+  const currentLeft = parseInt(getComputedStyle(document.querySelector(".project-container")).left, 10);
+  if (currentLeft < -270) {
+    //si el valor de izquierda es menor a -270, para de mover el contenido
     return;
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
   switch (newValue) {
     case -270:
-      document.querySelector('.project1').setAttribute("tabindex", "-1");
-      document.querySelector('.project1-container').setAttribute("aria-hidden", true);
-      document.querySelector('.project4').removeAttribute("tabindex");
-      document.querySelector('.project4-container').removeAttribute("aria-hidden")
+      document.querySelector(".project1").setAttribute("tabindex", "-1");
+      document.querySelector(".project1-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project4").removeAttribute("tabindex");
+      document.querySelector(".project4-container").removeAttribute("aria-hidden");
       break;
     case -540:
-      document.querySelector('.project2').setAttribute("tabindex", "-1");
-      document.querySelector('.project2-container').setAttribute("aria-hidden", "true");
-      document.querySelector('.project5').removeAttribute("tabindex");
-      document.querySelector('.project5-container').removeAttribute("aria-hidden");
+      document.querySelector(".project2").setAttribute("tabindex", "-1");
+      document.querySelector(".project2-container").setAttribute("aria-hidden", "true");
+      document.querySelector(".project5").removeAttribute("tabindex");
+      document.querySelector(".project5-container").removeAttribute("aria-hidden");
       break;
     default:
       break;
@@ -42,27 +38,25 @@ function clickRight() {
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
 function clickLeft() {
-  const currentLeft = parseInt(
-    getComputedStyle(document.querySelector(".project-container")).left,
-    10
-  );
-  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
+  const currentLeft = parseInt(getComputedStyle(document.querySelector(".project-container")).left, 10);
+  if (currentLeft === 0) {
+    //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
     return;
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
   switch (newValue) {
     case -270:
-      document.querySelector('.project5').setAttribute("tabindex", "-1");
-      document.querySelector('.project5-container').setAttribute("aria-hidden", "true");
-      document.querySelector('.project2').removeAttribute("tabindex");
-      document.querySelector('.project2-container').removeAttribute("aria-hidden");
+      document.querySelector(".project5").setAttribute("tabindex", "-1");
+      document.querySelector(".project5-container").setAttribute("aria-hidden", "true");
+      document.querySelector(".project2").removeAttribute("tabindex");
+      document.querySelector(".project2-container").removeAttribute("aria-hidden");
       break;
     case 0:
-      document.querySelector('.project4').setAttribute("tabindex", "-1");
-      document.querySelector('.project4-container').setAttribute("aria-hidden", "true");
-      document.querySelector('.project1').removeAttribute("tabindex");
-      document.querySelector('.project1-container').removeAttribute("aria-hidden");
+      document.querySelector(".project4").setAttribute("tabindex", "-1");
+      document.querySelector(".project4-container").setAttribute("aria-hidden", "true");
+      document.querySelector(".project1").removeAttribute("tabindex");
+      document.querySelector(".project1-container").removeAttribute("aria-hidden");
       break;
     default:
       break;
@@ -73,7 +67,7 @@ function clickLeft() {
 function validateForm(e) {
   e.preventDefault();
   const nameField = document.getElementById("name");
-  if (nameField.value === ""){
+  if (nameField.value === "") {
     document.getElementById("name-error").innerHTML = "! Para enviar el formulario, se necesita un nombre";
   } else {
     showNotification();
@@ -83,34 +77,31 @@ function validateForm(e) {
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
   document.getElementById("name-error").innerHTML = "";
-  document.querySelector('.form-container').reset();
+  document.querySelector(".form-container").reset();
   document.querySelector(".notification").style.display = "flex";
   document.querySelector(".notification").innerHTML = "El formulario fue enviado sin errores";
-  setTimeout(function() {
+  setTimeout(function () {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
 }
 
 /**Escucha por la clave esc para cerrar el modal */
 function listenForEsc(e) {
-  if (e.keyCode === 27){
-    closeModal(e)
+  if (e.keyCode === 27) {
+    closeModal(e);
   }
 }
 
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
-  document.getElementById('modal-header').focus();
+  document.getElementById("modal-header").focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
 function closeModal(e) {
   // si el click occurio dentro del las imagenes del carousel o dentro del modal, no se cierra el modal
-  if (
-    e.target.className.includes("project") ||
-    e.target.className === "modal"
-  ) {
+  if (e.target.className.includes("project") || e.target.className === "modal") {
     return;
   } else {
     document.querySelector(".modal-container").style.display = "none";
